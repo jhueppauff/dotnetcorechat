3ff83590dd54e0ae51f2507bf0b480f1c8fe1a37
Julian Hüpppauff <julian@hueppauff.info>
2018-07-18 22:26:12 +0200

    updated to use identies

--- Chat.Web/ChatHub.cs
+++ Chat.Web/ChatHub.cs
@@ -1,14 +1,21 @@
 using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace dotnetcorechat
 {
+    [Authorize]
     public class ChatHub : Hub
     {      
-        public async Task Send(string nick, string message)
+        public async Task Send(string message)
         {
-            await Clients.All.SendAsync("Send", nick, message);
+            await Clients.All.SendAsync("Send", Context.User.Identity.Name, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
         }
     }
 }
