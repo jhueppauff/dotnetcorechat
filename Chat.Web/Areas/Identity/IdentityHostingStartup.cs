81d6dcaff5c54a12f7d5ae6d75869db6cb25970e
Julian Hüpppauff <julian@hueppauff.info>
2018-07-19 22:54:27 +0200

    added scaffolding views, qrcode

---
+++ Chat.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using dotnetcorechat.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(dotnetcorechat.Areas.Identity.IdentityHostingStartup))]
+namespace dotnetcorechat.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
