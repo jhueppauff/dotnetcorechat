a0ac6f301ccfb2e86edc432405ed8bb4a41009e2
Julian Hüpppauff <julian@hueppauff.info>
2018-07-22 18:45:26 +0200

    added displayname to hub output

--- Chat.Web/ChatHub.cs
+++ Chat.Web/ChatHub.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Security.Claims;
 
 namespace dotnetcorechat
 {
@@ -10,17 +12,23 @@ namespace dotnetcorechat
     {      
         public async Task Send(string message)
         {
-            await Clients.All.SendAsync("Send", Context.User.Identity.Name, message);
+            var claims = Context.User.Claims;
+            string displayName = claims.FirstOrDefault(v => v.Type == ClaimTypes.GivenName).Value;
+            await Clients.All.SendAsync("Send", displayName, message);
         }
 
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("Join", Context.User.Identity.Name, "joined");
+            var claims = Context.User.Claims;
+            string displayName = claims.FirstOrDefault(v => v.Type == ClaimTypes.GivenName).Value;
+            await Clients.All.SendAsync("Join", displayName, "joined");
         }
 
         public override async Task OnDisconnectedAsync(Exception execption)
         {
-            await Clients.All.SendAsync("Leave", Context.User.Identity.Name, "joined");
+            var claims = Context.User.Claims;
+            string displayName = claims.FirstOrDefault(v => v.Type == ClaimTypes.GivenName).Value;
+            await Clients.All.SendAsync("Leave", displayName, "joined");
         }
     }
 }
